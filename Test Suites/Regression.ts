3f74a6fc3f733d66a4e03e284745d6434712c7af
RBC\900015183 <GRatnakar.Reddy@regalrexnord.com>
2022-06-14 18:18:04 +0530

    tc

---
+++ Test Suites/Regression.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>da336b1f-443d-4105-b94f-87545b27bdc7</testSuiteGuid>
+   <testCaseLink>
+      <guid>a7585caf-e80f-4b22-8e07-5e7f35dc96d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c111f9e2-142e-4f41-9716-62d9293bc0e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad610905-41c4-498a-9028-a62dbd957b7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
